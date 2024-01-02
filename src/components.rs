7fb0978dc5ba45739621e7918eaff48adca9928c
lsy969999 <lsy969999@gmail.com>
2024-01-02 23:27:37 +0900

    apply module

---
+++ src/components.rs
@@ -0,0 +1,33 @@
+use rltk::RGB;
+use specs::{System, ReadStorage, WriteStorage};
+use specs_derive::Component;
+use specs::prelude::*;
+#[derive(Component)]
+pub struct Position {
+  pub x: i32,
+  pub y: i32,
+}
+
+#[derive(Component)]
+pub struct Renderable {
+  pub glyph: rltk::FontCharType,
+  pub fg: RGB,
+  pub bg: RGB,
+}
+
+#[derive(Component, Debug)]
+pub struct Player {}
+
+#[derive(Component)]
+struct LeftMover{}
+
+struct LeftWalker {}
+impl <'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_, pos) in (&lefty, &mut pos).join() {
+          pos.x -= 1;
+          if pos.x < 0 {pos.x = 79;}
+        }
+    }
+}
