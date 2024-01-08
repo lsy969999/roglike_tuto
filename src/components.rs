8d28a1b47b899d74cf36cfb9c71d7828ce48f337
lsy969999 <lsy969999@gmail.com>
2024-01-08 12:20:28 +0900

    2.6. Dealing Damage

--- src/components.rs
+++ src/components.rs
@@ -1,48 +1,66 @@
-use rltk::RGB;
-use specs::{System, ReadStorage, WriteStorage};
-use specs_derive::Component;
 use specs::prelude::*;
+use specs_derive::*;
+use rltk::{RGB};
+
 #[derive(Component)]
 pub struct Position {
-  pub x: i32,
-  pub y: i32,
+    pub x: i32,
+    pub y: i32,
 }
 
 #[derive(Component)]
 pub struct Renderable {
-  pub glyph: rltk::FontCharType,
-  pub fg: RGB,
-  pub bg: RGB,
+    pub glyph: rltk::FontCharType,
+    pub fg: RGB,
+    pub bg: RGB,
 }
 
 #[derive(Component, Debug)]
 pub struct Player {}
 
 #[derive(Component)]
-struct LeftMover{}
-
-struct LeftWalker {}
-impl <'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_, pos) in (&lefty, &mut pos).join() {
-          pos.x -= 1;
-          if pos.x < 0 {pos.x = 79;}
-        }
-    }
-}
-
-#[derive(Component)]
 pub struct Viewshed {
-  pub visible_tiles: Vec<rltk::Point>,
-  pub range: i32,
-  pub dirty: bool,
+    pub visible_tiles : Vec<rltk::Point>,
+    pub range : i32,
+    pub dirty : bool
 }
 
 #[derive(Component, Debug)]
-pub struct Monster{}
+pub struct Monster {}
 
 #[derive(Component, Debug)]
 pub struct Name {
-  pub name: String
+    pub name : String
+}
+
+#[derive(Component, Debug)]
+pub struct BlocksTile {}
+
+#[derive(Component, Debug)]
+pub struct CombatStats {
+    pub max_hp : i32,
+    pub hp : i32,
+    pub defense : i32,
+    pub power : i32
+}
+
+#[derive(Component, Debug, Clone)]
+pub struct WantsToMelee {
+    pub target : Entity
+}
+
+#[derive(Component, Debug)]
+pub struct SufferDamage {
+    pub amount : Vec<i32>
+}
+
+impl SufferDamage {
+    pub fn new_damage(store: &mut WriteStorage<SufferDamage>, victim: Entity, amount: i32) {
+        if let Some(suffering) = store.get_mut(victim) {
+            suffering.amount.push(amount);
+        } else {
+            let dmg = SufferDamage { amount : vec![amount] };
+            store.insert(victim, dmg).expect("Unable to insert damage");
+        }
+    }
 }
